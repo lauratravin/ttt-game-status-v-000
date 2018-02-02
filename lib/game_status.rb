7a82a0a2b611903c320fdb7736fdf2b12e384109
Laura Messa Travin <laura.travin@gmail.com>
2018-02-02 06:57:32 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -47,7 +47,7 @@ end
 
 def over?(board)
   if won?(board) == true && full?(board) == true
-    return true
+    return false
   elsif  draw?(board) == true
     return true
   elsif won?(board) == true && full?(board) == false
